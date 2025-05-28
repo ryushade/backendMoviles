75543be00b3c3001f4045fa10f1fc9d44d4700da
ryushade <77291154@usat.pe>
2025-05-27 23:21:29 -0500

    Borrando tuplas

--- controller/usuario_controller.py
+++ controller/usuario_controller.py
@@ -68,8 +68,8 @@ def obtener_usuario_data():
     usuario = usuario_service.obtener_usuario_data_manga(email)
     if usuario:
         return jsonify({
-            "nombre": usuario[0],
-            "email": usuario[1]
+            "nombre": usuario['nombre_completo'],
+            "email": usuario['email']
         }), 200
     else:
         return jsonify({"msg": "Usuario no encontrado"}), 404
